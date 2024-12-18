4ac61bf9de602c4f0648645c76cb58aee37d203e
harshsoni003 <sonih3654@gmail.com>
2024-12-18 13:11:09 +0530

    Deploy updated build

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ModalProvider } from './context/modal-context';
 import { ThemeProvider } from './context/theme-context';
 import './index.css'
 
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThemeProvider>
     <ModalProvider>
-        <App/>
+        <App />
     </ModalProvider>
     </ThemeProvider>
 );
